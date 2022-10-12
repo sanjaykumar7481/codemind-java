931c1b020bc4978289a104621e4ef0fbe12cd20b
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-10-12 11:57:14 +0530

    import java.util.Scanner; class Main{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         String a=n.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args)
+    {
+        Scanner n=new Scanner(System.in);
+        String a=n.nextLine();
+        System.out.println(a);
+    }
+}
