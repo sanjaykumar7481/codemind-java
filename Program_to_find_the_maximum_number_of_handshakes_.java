9a008128111105386e6c556b76777c30095c7162
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-10-12 13:39:30 +0530

    import java.util.Scanner; class Main{     public static void main(String[] args){         Scanner i=new Scanner(System.in);         int a=i.nextInt();         System.out.println((a-1)*a/2);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner i=new Scanner(System.in);
+        int a=i.nextInt();
+        System.out.println((a-1)*a/2);
+    }
+}
