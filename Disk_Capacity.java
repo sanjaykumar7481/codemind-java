8a58619a5067772b8b1221a2d6a07f3f17c060b9
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-10-12 13:57:38 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner n=new Scanner(System.in);         int a=n.nextInt();         int b=n.nextInt();         int c=n.nextInt();         System.out.println(a*b*c*512*2);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner n=new Scanner(System.in);
+        int a=n.nextInt();
+        int b=n.nextInt();
+        int c=n.nextInt();
+        System.out.println(a*b*c*512*2);
+    }
+}
