3d51af81dde3705c8b2c07b0ec8c563bfb92676c
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-10-14 22:41:18 +0530

    import java.util.*; class Main{ public static void main(String args[]) {  Scanner sc=new Scanner(System.in);  int count=sc.nextInt();  int k=1;  for(int i=1;i<=count;i++)  {     for(int j=1;j<=count;j++)     {         if(j==k||j==count-k+1)         System.out.print("x"+"");         else         System.out.print("0"+"");     }     k++;     System.out.print(" ");  } } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,15 +4,17 @@ public static void main(String args[])
 {   
  Scanner sc=new Scanner(System.in);
  int count=sc.nextInt();
+ int k=1;
  for(int i=1;i<=count;i++)
  {    
     for(int j=1;j<=count;j++)
     {
-        if(j==1||i==j||j==count)
-        System.out.print("*"+" ");
+        if(j==k||j==count-k+1)
+        System.out.print("x"+"");
         else
-        System.out.print(" "+" ");
+        System.out.print("0"+"");
     }
+    k++;
     System.out.print("
 ");
  }
