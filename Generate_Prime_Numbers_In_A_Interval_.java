4aea707dd13fb69cb193656af4350d60fd3c8288
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-10-21 14:28:42 +0530

    import java.util.*; class Main{     static int prime(int n)     {         if(n==0)         return 0;         if(n==1)         return 0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             return 0;         }         return 1;     }     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=0;         for(int i=a;i<=b;i++)         {             if(prime(i)==1)             System.out.println(i);         }     } }

---
+++ Generate_Prime_Numbers_In_A_Interval_.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main{
+    static int prime(int n)
+    {
+        if(n==0)
+        return 0;
+        if(n==1)
+        return 0;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            return 0;
+        }
+        return 1;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=0;
+        for(int i=a;i<=b;i++)
+        {
+            if(prime(i)==1)
+            System.out.println(i);
+        }
+    }
+}
